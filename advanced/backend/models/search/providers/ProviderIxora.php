62b7d722fe4034e9e65229a88b0f4ccf6918d281
Sync server <sales@atc58.ru>
2016-02-05 15:25:33 +0400

    fix Provider Ixora

--- advanced/backend/models/search/providers/ProviderIxora.php
+++ advanced/backend/models/search/providers/ProviderIxora.php
@@ -11,78 +11,62 @@ class ProviderIxora extends Provider{
 
   public function getBrands($search_text, $use_analog) {
     $data= ['Number'=>$search_text];
-    return $this->prepareRequest($data,true,  $this->_url."/GetMakersXML");
+    return $this->prepareRequest($data, true,  $this->_url."/GetMakersXML");
   }
 
   public function getBrandsParse($xml) {
-    var_dump($xml);
-    /*$field = $this->getRowName();
+    $field = $this->getRowName();
     if( !is_array($xml) || !isset($xml[$field]) ){
       return [];
     }
     $data     = $xml[$field];
-    if( isset($data['producer']) ){
-      $data = [$data];
-    }*/
     $answer   = [];
-    //foreach ($data as $row){
-    //  $maker  = strtoupper($row['producer']);
-    //  $maker  = preg_replace('/\W*/i', "", $maker);
-    //  $answer[ $maker ] = ['id' => $this->getCLSID(),'uid' => $row['ident']];
-    //} 
+    foreach ($data as $row){
+      $maker  = strtoupper($row['name']);
+      $maker  = preg_replace('/\W*/i', "", $maker);
+      $answer[ $maker ] = ['id' => $this->getCLSID(),'uid' => $row['id'] . '@@' . $row['name']];
+    } 
     return $answer;
   }
   
   public function getParts($ident) {
     list($code,$maker) = explode('@@', $ident);
     $data   = ['Maker'=>$maker,'Number'=>$code,'StockOnly'=>'false','SubstFilter'=>'All'];
-    $reqest = $this->prepareRequest($data);
+    $reqest = $this->prepareRequest($data,true, $this->_url."/FindXML");
 
     $answer = $this->executeRequest($reqest);
-    
     $array  = $this->xmlToArray($answer);
-    /*if( isset($array['uid']) ){
-      $array = ['detail' => $array];
+    if( !isset($array['DetailInfo']) ){
+			return [];
     }
-
-    $data   = $array[$this->getRowName()];
-    */
+		
+    $data   = $array['DetailInfo'];
     $result = [];
-    /*foreach ($data as $item){
+    foreach ($data as $item){
       $converted = $this->renameByMap($item, $this->getNamesMap());
-      if( is_array($converted["lot_quantity"]) ){
-        $converted["lot_quantity"] = 0;
-      }
-      if( is_array($converted["is_original"]) ){
-        $converted["is_original"] = false;
-      }
-      if( is_array($converted["name"]) ){
-        $converted["name"] = "--";
-      }
-
+			$converted['is_original'] = ($converted['is_original']==='Analog')?false:true;
       $result[] = $converted;
-    }*/
+    }
     return $result;    
   }
 
   protected function getNamesMap() {
     return [
-      "code"      => "articul",
-      "producer"  => "maker",
-      "uid"       => "code",
-      "caption"   => "name",
-      "price"     => "price",
-      "deliverydays" => "shiping",
-      "stock"     => "stock",
-      "stockinfo" => "info",
-      "analog"    => "is_original",
-      "rest"      => "count",
-      "amount"    => "lot_quantity"
+      "number"      		=> "articul",
+      "maker"  					=> "maker",
+      "orderreference"  => "code",
+      "name"   					=> "name",
+      "price"     			=> "price",
+      "dayswarranty" 		=> "shiping",
+      "region"     			=> "stock",
+      "group"    				=> "is_original",
+      "quantity"	      => "count",
+      "lotquantity"    	=> "lot_quantity"
     ];
   }
 
   protected function getRowName() {
-    return 'detail';
+    return 'MakerInfo';
   }
 
 }
