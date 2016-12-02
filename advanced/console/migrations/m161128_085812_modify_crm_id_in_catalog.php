19b31c2ab9a1e006bf5d3cddd14fcdf934805786
Sync1983 <1983sync@gmail.com>
2016-12-02 14:42:41 +0300

    fix parts-list layout

---
+++ advanced/console/migrations/m161128_085812_modify_crm_id_in_catalog.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\db\Migration;
+
+class m161128_085812_modify_crm_id_in_catalog extends Migration{
+
+ public function up(){
+   $this->alterColumn('Catalog', 'crm_id' , 'bigint');
+ }
+
+ public function down() {
+   $this->alterColumn('Catalog', 'crm_id' , 'bigint');
+ }
+}
---
+++ advanced/console/migrations/m161128_093718_add_catalog_functions.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Migration;
+
+class m161128_093718_add_catalog_functions extends Migration{
+
+  public function up(){
+    $SQL = <<<SQL
+        CREATE OR REPLACE FUNCTION get_catalog_group(IN p text, IN g boolean)
+          RETURNS TABLE(id bigint, name character varying, path text) AS $$
+          SELECT "id","name",CAST("path" as text) FROM "Catalog" WHERE path ~ CAST(p || '.*{1}' AS lquery) and is_group = g;
+        $$ LANGUAGE sql;
+SQL;
+    $this->execute($SQL);
+  }
+
+ public function down() {
+     $SQL = <<<SQL
+       DROP FUNCTION IF EXISTS get_catalog_group()
+SQL;
+    return $this->execute($SQL);
+ }
+ 
+}
